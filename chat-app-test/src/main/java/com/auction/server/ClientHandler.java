19106649895403512cc08946dfdce816664c32c9
rusinit00-art <rusinit00@gmail.com>
2026-06-09 12:41:12 +0530

    add all

--- chat-app-test/src/main/java/com/auction/server/ClientHandler.java
+++ chat-app-test/src/main/java/com/auction/server/ClientHandler.java
@@ -24,7 +24,6 @@ public class ClientHandler implements Runnable {
         this.server = server;
         this.logger = logger;
     }
-
     public String getUsername() {
         return username;
     }
